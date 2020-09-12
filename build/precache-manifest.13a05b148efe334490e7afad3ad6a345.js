e398ed03c1704cb67af98ba5dfdc8f18af788fed
kartik malik <kartikforhack@gmail.com>
2020-09-13 04:10:30 +0530

    asdf

---
+++ build/precache-manifest.13a05b148efe334490e7afad3ad6a345.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea062e3dca8dca3462fef0bdf3f3c5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5940f48d52d651d107d",
+    "url": "/static/css/main.34c0e548.chunk.css"
+  },
+  {
+    "revision": "860074e8a12acd81f090",
+    "url": "/static/js/2.b98a593f.chunk.js"
+  },
+  {
+    "revision": "37387729046d627b56780cf21eb991bb",
+    "url": "/static/js/2.b98a593f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5940f48d52d651d107d",
+    "url": "/static/js/main.91d83073.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "75914da431da85fefbcc7fd9afb7d9c1",
+    "url": "/static/media/Capture.75914da4.PNG"
+  },
+  {
+    "revision": "b8d66617682d0eb8d2c8a042873a8ada",
+    "url": "/static/media/dashboard.b8d66617.svg"
+  },
+  {
+    "revision": "37a56acc8af5af7cb47aa748c7415dc3",
+    "url": "/static/media/doc.37a56acc.svg"
+  },
+  {
+    "revision": "224785549b941803a14de81c8687dcba",
+    "url": "/static/media/document.22478554.svg"
+  },
+  {
+    "revision": "0d5203b3c05f450bc536966c55f659bd",
+    "url": "/static/media/home.0d5203b3.svg"
+  },
+  {
+    "revision": "d511ba802f827d6aa79ca5c0c51df531",
+    "url": "/static/media/img.d511ba80.svg"
+  },
+  {
+    "revision": "d2feca42237e6c9fe4eab2dfc57096f5",
+    "url": "/static/media/installation.d2feca42.svg"
+  },
+  {
+    "revision": "b2c48d48c17c105f4dd3f60b7430e384",
+    "url": "/static/media/letterHead.b2c48d48.png"
+  },
+  {
+    "revision": "5e3fd4f1a1553286f2effbde5a88a950",
+    "url": "/static/media/logo-full.5e3fd4f1.png"
+  },
+  {
+    "revision": "f151a60928566857ae9717304a6f0f7c",
+    "url": "/static/media/logoicon.f151a609.png"
+  },
+  {
+    "revision": "876fb2da38ca570459642900b78b65a8",
+    "url": "/static/media/logotext.876fb2da.png"
+  },
+  {
+    "revision": "19dcde0fac370dd9e65abb69b2db7400",
+    "url": "/static/media/machinery.19dcde0f.svg"
+  },
+  {
+    "revision": "5b1cb73fa42ab6e0fd09676aa786816e",
+    "url": "/static/media/material.5b1cb73f.svg"
+  },
+  {
+    "revision": "0f699da90cd660fbbaeff337a294bdfc",
+    "url": "/static/media/menu.0f699da9.svg"
+  },
+  {
+    "revision": "7d7b22ee63b0485a61b9af1ff8d57593",
+    "url": "/static/media/pdf.7d7b22ee.svg"
+  },
+  {
+    "revision": "9840dacf6a0760d09e9198a661837fbf",
+    "url": "/static/media/project.9840dacf.svg"
+  },
+  {
+    "revision": "9039b8ea15901dda1d55f37f1d01ca8b",
+    "url": "/static/media/user.9039b8ea.svg"
+  },
+  {
+    "revision": "cabce7165ca85b1c3842b78dbc0dac3a",
+    "url": "/static/media/users.cabce716.svg"
+  },
+  {
+    "revision": "0fa2dfe99be10ab69e8b43717e982630",
+    "url": "/static/media/vendor.0fa2dfe9.svg"
+  },
+  {
+    "revision": "8e124ac750ee8e60b2fcd5857681d3e3",
+    "url": "/static/media/vendor1.8e124ac7.svg"
+  }
+]);
